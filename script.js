29cd18e4fa8076c7a6d2e7a8329b2c549c9f5e90
tribhuvanj-glitch <tribhuvanjeeprepiit@gmail.com>
2026-03-09 22:29:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ document.getElementById(id).scrollIntoView({
 behavior:"smooth"
 });
 }
+
+const observer=new IntersectionObserver(entries=>{
+entries.forEach(entry=>{
+if(entry.isIntersecting){
+entry.target.classList.add("active");
+}
+});
+});
+
+document.querySelectorAll(".reveal").forEach(el=>{
+observer.observe(el);
+});
