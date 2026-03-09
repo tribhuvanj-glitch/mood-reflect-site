1f96a73a2f56f45aa4c2325ff49c9d57d7435d0a
tribhuvanj-glitch <tribhuvanjeeprepiit@gmail.com>
2026-03-09 22:14:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// optional JS file for future enhancements
+function downloadApp(){
+
+alert("Download coming soon");
+
+}
+
+function scrollToDemo(){
+
+document.getElementById("demo").scrollIntoView({
+
+behavior:"smooth"
+
+});
+
+}// optional JS file for future enhancements
